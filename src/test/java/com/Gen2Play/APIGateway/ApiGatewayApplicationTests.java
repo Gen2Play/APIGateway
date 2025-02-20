ebd9c081b38b44c6c40f740eaef06d0e8307d07f
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-20 23:55:46 +0700

    commit api gateway

---
+++ src/test/java/com/Gen2Play/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gen2Play.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
