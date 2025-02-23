e1c1b8fda7077fdbe2dcefb3b0430ef09be1bacc
Vo Thanh Tuyen <thanhtuyen66202@gmail.com>
2025-02-24 00:14:53 +0700

    add Dockerfile

--- src/main/java/com/Gen2Play/APIGateway/configuration/EnvConfig.java
+++ src/main/java/com/Gen2Play/APIGateway/configuration/EnvConfig.java
@@ -1,21 +1,22 @@
 package com.Gen2Play.APIGateway.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class EnvConfig {
 
-    private final Dotenv dotenv = Dotenv.load();
+    // private final Dotenv dotenv = Dotenv.load();
 
-    @Bean
-    public Dotenv dotenv() {
-        return dotenv;
-    }
+    // @Bean
+    // public Dotenv dotenv() {
+    //     return dotenv;
+    // }
 
-    public String getEnv(String key) {
-        return dotenv.get(key);
+    // public String getEnv(String key) {
+    //     return dotenv.get(key);
+    // }
+
+    public static String getEnv(String key) {
+        return System.getenv(key);
     }
 }
